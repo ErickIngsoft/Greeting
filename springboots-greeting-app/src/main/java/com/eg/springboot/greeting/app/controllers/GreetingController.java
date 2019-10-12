da507f932f8a95f10b2f9f90a3764a19170382da
ErickIngsoft <54635370+ErickIngsoft@users.noreply.github.com>
2019-10-12 13:43:34 +0200

    Update GreetingController.java

--- springboots-greeting-app/src/main/java/com/eg/springboot/greeting/app/controllers/GreetingController.java
+++ springboots-greeting-app/src/main/java/com/eg/springboot/greeting/app/controllers/GreetingController.java
@@ -1,7 +1,6 @@
 package com.eg.springboot.greeting.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
